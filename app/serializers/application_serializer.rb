b0271ac9b6a9d1e1c1570865a4b5e74a0846e7ce
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2024-07-05 11:23:53 +0300

    Add chats_count to application and seeder for application model

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,4 +1,4 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  attributes :name, :token, :created_at, :updated_at
+  attributes :name, :token, :chats_count, :created_at, :updated_at
 end
 
--- db/migrate/20240704140950_create_applications.rb
+++ db/migrate/20240704140950_create_applications.rb
@@ -3,7 +3,7 @@ class CreateApplications < ActiveRecord::Migration[7.1]
     create_table :applications do |t|
       t.string :name
       t.string :token, null: false, limit: 16
-
+      t.integer :chats_count, default: 0
       t.timestamps
     end
 
