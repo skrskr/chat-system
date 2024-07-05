c90f7595ae58d385252bbe9a5fb2c9a1f797eade
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2024-07-05 12:17:25 +0300

    Add chat model with migrations

---
+++ app/models/message.rb
@@ -0,0 +1,5 @@
+class Message < ApplicationRecord
+  validates :body, presence: true
+  
+  belongs_to :chat
+end
---
+++ db/migrate/20240705091543_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration[7.1]
+  def change
+    create_table :messages do |t|
+      t.integer :number
+      t.text :body
+      t.references :chat, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
